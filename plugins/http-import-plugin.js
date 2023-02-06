cee7ce830bed63f1de095bcc3c0f96679b2431e4
luokang <1023349760@qq.com>
2023-02-06 18:41:49 +0800

    feat: init

--- plugins/http-import-plugin.js
+++ plugins/http-import-plugin.js
@@ -1,10 +1,67 @@
 module.exports = () => ({
-  name: 'CDN',
+  name: 'esbuild:http',
   setup(build) {
-    build.onStart(() => {});
-    build.onEnd((buildResult) => {});
+    let https = require('https');
+    let http = require('http');
 
-    build.onResolve({}, (args) => {});
-    build.onLoad({}, (args) => {});
+    // 1.拦截cdn解析
+    build.onResolve(
+      // 匹配过滤http开头的模块
+      { filter: /^https?:\/\// },
+      (args) => {
+        return {
+          // 保留原路径
+          path: args.path,
+          // 添加namespace标识
+          namespace: 'http-url',
+        };
+      }
+    );
+
+    // 2. fetch请求加载cdn资源
+    build.onLoad(
+      {
+        filter: /.*/,
+        namespace: 'http-url',
+      },
+      async (args) => {
+        let contents = await new Promise((resolve, reject) => {
+          function fetch(url) {
+            console.log(`Downloading:${url}`);
+
+            let lib = url.startsWith('https') ? https : http;
+            let req = lib
+              .get(url, (res) => {
+                //响应请求
+                const {
+                  statusCode,
+                  headers: { location },
+                } = res;
+
+                if ([301, 302, 307].includes(statusCode)) {
+                  // 重定向
+                  fetch(new URL(location, url).toString());
+                  req.destroy();
+                } else if (statusCode === 200) {
+                  // 响应成功
+                  let chunks = [];
+                  // 接收数据
+                  res.on('data', (chunk) => chunks.push(chunk));
+                  // 结束合并数据并返回
+                  res.end('end', () => resolve(Buffer.concat(chunks)));
+                } else {
+                  // 请求失败
+                  reject(new Error(`GET ${url} failed : status ${statusCode}`));
+                }
+              })
+              .on('error', reject);
+          }
+
+          fetch(ar.path);
+        });
+
+        return { contents };
+      }
+    );
   },
 });
